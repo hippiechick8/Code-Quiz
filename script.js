aa25ac664b7306614b06742013e298df9fe11b09
hippiechick8 <57959762+hippiechick8@users.noreply.github.com>
2020-01-06 18:21:47 -0600

    listed html elements as variables

---
+++ script.js
@@ -0,0 +1,8 @@
+// list of html elements
+var highScores = document.getElementsByClassName("highScores");
+var timeRemanining = document.getElementsByClassName("timeRemaining");
+var codeQuiz = document.getElementsByClassName("codeQuiz");
+var quizInstruct = document.getElementById("quizInstruct");
+var mainQuiz = document.getElementById("mainQuiz");
+var buttons = document.getElementsByClassName("buttons");
+var startQuiz = document.getElementById("startQuiz");
