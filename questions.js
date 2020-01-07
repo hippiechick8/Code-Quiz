30c0d760aa34b78774003d3d6f0ce08cc1303af0
hippiechick8 <57959762+hippiechick8@users.noreply.github.com>
2020-01-06 18:08:59 -0600

    initial commit with questions and answers

---
+++ questions.js
@@ -0,0 +1,23 @@
+// questions and answers for Code Quiz
+var questions = [
+  {
+    title: 'Which item below is one of quickest ways to double check that code is functioning correctly as it is being written?',
+    choices: ['GitHub', 'jQuery', 'Ctrl + s', 'console.log'],
+    answer: 'console.log'
+  },
+  { 
+    title: 'Using the modulo operator (%) returns a number\'s ______.',
+    choices: ['decimal', 'remainder', 'Boolean', 'sum'],
+    answer: 'remainder'
+  },
+  { 
+    title: 'Which answer below lists three of the seven JavaScript Primitives?',
+    choices: ['string, Boolean, number', 'Boolean, number, repository', 'console, bigInt, Boolean', 'number, null, curly braces'],
+    answer: 'string, Boolean, number'
+  },
+  { 
+    title: 'GitHub is an online repository to store code and is a _______.',
+    choices: ['central place to view code history and track changes', 'central spot for master files', 'both answers', 'neither answer'],
+    answer: 'both answers'
+  },
+];
